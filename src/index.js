07a757f40b74b82d8ecfdad19d663b0989edac4f
Александр Кормич <stud0000227669@study.utmn.ru>
2023-03-04 18:59:43 +0500

    lesson(7) finished (сверстан для трех устройств)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log(document.getElementById('test-id'))
-// })
+// 1
+function pickPropArray (mass,prop){
+    const newmas =[];
+    for(let i=0; i< mass.length;i++)
+    {
+        const obj = mass[i];
+        if (obj.key === prop) newmas.push(obj.value)
+    }
+    return newmas;
+}
+// 2
+function createCounter() {
+    let count =0;
+    return function () {
+        ++count;
+    }
+}
+// 3
+const spinWords = (str) => {
+    return str
+        .split('')
+        .map(el=> (el === ' ') ? el : '')
+        .join('')
+        .split(' ')
+        .map(el => (el.length >= 5) ? el.split('').reverse().join('') : el)
+        .join(' ')
+}
